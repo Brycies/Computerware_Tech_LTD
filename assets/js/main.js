5eb2abfb94973a33413f8ae7cf58ccd9f989d55c
Brycies <brysonodhiambo29@gmail.com>
2023-10-28 10:55:36 +0300

    whatsApp chatbot

--- assets/js/main.js
+++ assets/js/main.js
@@ -232,89 +232,122 @@
 /**
  * Chatbox Script
  */
-const chatbotToggler = document.querySelector(".chatbot-toggler");
-const closeBtn = document.querySelector(".close-btn");
-const chatbox = document.querySelector(".chatbox");
-const chatInput = document.querySelector(".chat-input textarea");
-const sendChatBtn = document.querySelector(".chat-input span");
-let userMessage = null; // Variable to store user's message
-const API_KEY = "PASTE-YOUR-API-KEY"; // Paste your API key here
-const inputInitHeight = chatInput.scrollHeight;
-const createChatLi = (message, className) => {
-  // Create a chat <li> element with passed message and className
-  const chatLi = document.createElement("li");
-  chatLi.classList.add("chat", `${className}`);
-  let chatContent =
-    className === "outgoing"
-      ? `<p></p>`
-      : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
-  chatLi.innerHTML = chatContent;
-  chatLi.querySelector("p").textContent = message;
-  return chatLi; // return chat <li> element
-};
-const generateResponse = (chatElement) => {
-  const API_URL = "https://api.openai.com/v1/chat/completions";
-  const messageElement = chatElement.querySelector("p");
-  // Define the properties and message for the API request
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${API_KEY}`,
-    },
-    body: JSON.stringify({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: userMessage }],
-    }),
-  };
-  // Send POST request to API, get response and set the reponse as paragraph text
-  fetch(API_URL, requestOptions)
-    .then((res) => res.json())
-    .then((data) => {
-      messageElement.textContent = data.choices[0].message.content.trim();
-    })
-    .catch(() => {
-      messageElement.classList.add("error");
-      messageElement.textContent =
-        "Oops! Something went wrong. Please try again.";
-    })
-    .finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
-};
-const handleChat = () => {
-  userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
-  if (!userMessage) return;
-  // Clear the input textarea and set its height to default
-  chatInput.value = "";
-  chatInput.style.height = `${inputInitHeight}px`;
-  // Append the user's message to the chatbox
-  chatbox.appendChild(createChatLi(userMessage, "outgoing"));
-  chatbox.scrollTo(0, chatbox.scrollHeight);
+// const chatbotToggler = document.querySelector(".chatbot-toggler");
+// const closeBtn = document.querySelector(".close-btn");
+// const chatbox = document.querySelector(".chatbox");
+// const chatInput = document.querySelector(".chat-input textarea");
+// const sendChatBtn = document.querySelector(".chat-input span");
+// let userMessage = null; // Variable to store user's message
+// const API_KEY = "PASTE-YOUR-API-KEY"; // Paste your API key here
+// const inputInitHeight = chatInput.scrollHeight;
+// const createChatLi = (message, className) => {
+//   // Create a chat <li> element with passed message and className
+//   const chatLi = document.createElement("li");
+//   chatLi.classList.add("chat", `${className}`);
+//   let chatContent =
+//     className === "outgoing"
+//       ? `<p></p>`
+//       : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+//   chatLi.innerHTML = chatContent;
+//   chatLi.querySelector("p").textContent = message;
+//   return chatLi; // return chat <li> element
+// };
+// const generateResponse = (chatElement) => {
+//   const API_URL = "https://api.openai.com/v1/chat/completions";
+//   const messageElement = chatElement.querySelector("p");
+//   // Define the properties and message for the API request
+//   const requestOptions = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${API_KEY}`,
+//     },
+//     body: JSON.stringify({
+//       model: "gpt-3.5-turbo",
+//       messages: [{ role: "user", content: userMessage }],
+//     }),
+//   };
+//   // Send POST request to API, get response and set the reponse as paragraph text
+//   fetch(API_URL, requestOptions)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       messageElement.textContent = data.choices[0].message.content.trim();
+//     })
+//     .catch(() => {
+//       messageElement.classList.add("error");
+//       messageElement.textContent =
+//         "Oops! Something went wrong. Please try again.";
+//     })
+//     .finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+// };
+// const handleChat = () => {
+//   userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
+//   if (!userMessage) return;
+//   // Clear the input textarea and set its height to default
+//   chatInput.value = "";
+//   chatInput.style.height = `${inputInitHeight}px`;
+//   // Append the user's message to the chatbox
+//   chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+//   chatbox.scrollTo(0, chatbox.scrollHeight);
 
-  setTimeout(() => {
-    // Display "Thinking..." message while waiting for the response
-    const incomingChatLi = createChatLi("Thinking...", "incoming");
-    chatbox.appendChild(incomingChatLi);
-    chatbox.scrollTo(0, chatbox.scrollHeight);
-    generateResponse(incomingChatLi);
-  }, 600);
-};
-chatInput.addEventListener("input", () => {
-  // Adjust the height of the input textarea based on its content
-  chatInput.style.height = `${inputInitHeight}px`;
-  chatInput.style.height = `${chatInput.scrollHeight}px`;
-});
-chatInput.addEventListener("keydown", (e) => {
-  // If Enter key is pressed without Shift key and the window
-  // width is greater than 800px, handle the chat
-  if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
-    e.preventDefault();
-    handleChat();
-  }
-});
-sendChatBtn.addEventListener("click", handleChat);
-closeBtn.addEventListener("click", () =>
-  document.body.classList.remove("show-chatbot")
-);
-chatbotToggler.addEventListener("click", () =>
-  document.body.classList.toggle("show-chatbot")
-);
+//   setTimeout(() => {
+//     // Display "Thinking..." message while waiting for the response
+//     const incomingChatLi = createChatLi("Thinking...", "incoming");
+//     chatbox.appendChild(incomingChatLi);
+//     chatbox.scrollTo(0, chatbox.scrollHeight);
+//     generateResponse(incomingChatLi);
+//   }, 600);
+// };
+// chatInput.addEventListener("input", () => {
+//   // Adjust the height of the input textarea based on its content
+//   chatInput.style.height = `${inputInitHeight}px`;
+//   chatInput.style.height = `${chatInput.scrollHeight}px`;
+// });
+// chatInput.addEventListener("keydown", (e) => {
+//   // If Enter key is pressed without Shift key and the window
+//   // width is greater than 800px, handle the chat
+//   if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+//     e.preventDefault();
+//     handleChat();
+//   }
+// });
+// sendChatBtn.addEventListener("click", handleChat);
+// closeBtn.addEventListener("click", () =>
+//   document.body.classList.remove("show-chatbot")
+// );
+// chatbotToggler.addEventListener("click", () =>
+//   document.body.classList.toggle("show-chatbot")
+// );
+
+/**
+ * WhatsAppChatbox Script
+ */
+
+document
+  .getElementById("whats-chat")
+  .addEventListener("mouseover", showchatbox);
+document
+  .getElementById("chat-top-right")
+  .addEventListener("click", closechatbox);
+document.getElementById("send-btn").addEventListener("click", sendmsg);
+window.addEventListener("load", showchatboxtime);
+function showchatbox() {
+  document.getElementById("chat-box").style.right = "8%";
+}
+function closechatbox() {
+  document.getElementById("chat-box").style.right = "-500px";
+}
+function showchatboxtime() {
+  setTimeout(launchbox, 5000);
+}
+function launchbox() {
+  document.getElementById("chat-box").style.right = "8%";
+}
+function sendmsg() {
+  var msg = document.getElementById("whats-in").value;
+  var relmsg = msg.replace(/ /g, "%20");
+  window.open(
+    "https://api.whatsapp.com/send?phone=99999999999&text=" + relmsg,
+    "_blank"
+  );
+}
